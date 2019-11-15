3f5fb3b071c823b7b98f9fabdac087dbffc16c32
Ethan <esneddon@mymail.mines.edu>
2019-11-14 23:01:47 -0700

    created file that calculates the first 10 fibonacci numbers

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+# fibonacci.py
+fib_num = int(0)
+def fib():
+    fibs = [1, 2]
+    
+    for i in range(1,9):
+        fibs.append(fibs[i] + fibs[i-1])
+        
+
+    return fibs
+
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
